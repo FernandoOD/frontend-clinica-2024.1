ae10fe614956d22add8674c3ba48ce63ad8a8d55
FernandoOD <ferchuy2001@gmail.com>
2024-09-27 18:04:17 -0600

    Creacion de los modulos

---
+++ src/app/modulos/terapeuta/terapeuta.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TerapeutaRoutingModule } from './terapeuta-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TerapeutaRoutingModule
+  ]
+})
+export class TerapeutaModule { }
