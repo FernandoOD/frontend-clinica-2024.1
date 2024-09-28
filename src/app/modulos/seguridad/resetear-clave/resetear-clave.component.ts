1966d63bac55984f022cdf2ba7d2892c1b687345
FernandoOD <ferchuy2001@gmail.com>
2024-09-27 18:12:16 -0600

    Creacion de componentes para los modulos de seguridad y usuario

---
+++ src/app/modulos/seguridad/resetear-clave/resetear-clave.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resetear-clave',
+  standalone: true,
+  imports: [],
+  templateUrl: './resetear-clave.component.html',
+  styleUrl: './resetear-clave.component.css'
+})
+export class ResetearClaveComponent {
+
+}
